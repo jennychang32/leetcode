de09c5485191d01353e44b517eef44fa8010122c
jennychang32 <101952250+jennychang32@users.noreply.github.com>
2022-04-13 19:04:27 -0400

    Create 9. Palindrome Number.py
    Runtime: 82 ms, faster than 60.97% of Python3 online submissions for Palindrome Number.
    Memory Usage: 13.9 MB, less than 63.92% of Python3 online submissions for Palindrome Number.

---
+++ python/9. Palindrome Number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        if x<0 or (x%10==0 and x!=0):
+            return False
+        
+        elif x==0:
+            return True
+        
+        else:
+            
+            bound=0
+            while (x>bound):
+                bound = bound * 10 + x%10
+                x //= 10
+            
+            print(x, bound)
+            if x==bound or x==bound//10:
+                return True
+            else:
+                return False
