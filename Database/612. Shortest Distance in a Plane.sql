c292a67c92a7312d45bb46bbe4f49d2cd7d56778
jennychang32 <101952250+jennychang32@users.noreply.github.com>
2022-04-17 14:36:45 -0400

    Update 612. Shortest Distance in a Plane.sql
    Runtime: 249 ms, faster than 50.70% of MySQL online submissions for Shortest Distance in a Plane.
    Memory Usage: 0B, less than 100.00% of MySQL online submissions for Shortest Distance in a Plane.

--- Database/612. Shortest Distance in a Plane.sql
+++ Database/612. Shortest Distance in a Plane.sql
@@ -6,3 +6,13 @@ Join (
     Select x as x1, y as y1
         From Point2D) as Point2D1
 Where Sqrt(Pow(x-x1,2)+Pow(y-y1,2))<>0
+
+/* Method 2 */
+
+Select round(sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y)),2) as shortest
+    From Point2D
+Join
+(Select x as x1, y as y1
+    From Point2D) as Point2D1
+Where sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y)) >0
+Order by sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y)) asc limit 1
